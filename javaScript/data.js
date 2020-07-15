57997ef5f645c4aacdf1bdd0b9581d91f6b9a3c9
nahida8 <sakisnahida@gmail.com>
2020-07-15 23:31:49 +0300

    data connection

---
+++ javaScript/data.js
@@ -0,0 +1,23 @@
+function GetPatient() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+    }
+  };
+  xhttp.open("POST", "Your Rest URL Here", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send("Your JSON Data Here");
+}
+
+function AddPatient() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+    }
+  };
+  xhttp.open("POST", "Your Rest URL Here", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send("Your JSON Data Here");
+}
